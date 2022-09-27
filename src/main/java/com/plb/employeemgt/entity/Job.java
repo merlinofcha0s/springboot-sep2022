7e400e509701d0a45d2d9f5ce3c9a38f3d338939
Cyril Casaucau <casaucau.cyril@protonmail.com>
2022-09-27 12:04:57 +0200

    Add job and tasks entity

---
+++ src/main/java/com/plb/employeemgt/entity/Job.java
@@ -0,0 +1,113 @@
+package com.plb.employeemgt.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * A Job.
+ */
+@Entity
+@Table(name = "job")
+public class Job implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "jobSequenceGenerator")
+    @SequenceGenerator(name = "jobSequenceGenerator")
+    private Long id;
+
+    @Column(name = "job_title")
+    private String jobTitle;
+
+    @Column(name = "min_salary")
+    private Long minSalary;
+
+    @Column(name = "max_salary")
+    private Long maxSalary;
+
+    @ManyToMany
+    @JoinTable(name = "job_task",
+               joinColumns = @JoinColumn(name = "job_id", referencedColumnName = "id"),
+               inverseJoinColumns = @JoinColumn(name = "task_id", referencedColumnName = "id"))
+    private Set<Task> tasks = new HashSet<>();
+
+    @ManyToOne
+    private Employee employee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public Long getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(Long minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public Long getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(Long maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return Objects.equals(jobTitle, job.jobTitle) &&
+                Objects.equals(minSalary, job.minSalary) &&
+                Objects.equals(maxSalary, job.maxSalary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(jobTitle, minSalary, maxSalary);
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+            "id=" + getId() +
+            ", jobTitle='" + getJobTitle() + "'" +
+            ", minSalary=" + getMinSalary() +
+            ", maxSalary=" + getMaxSalary() +
+            "}";
+    }
+}
---
+++ src/main/java/com/plb/employeemgt/entity/Task.java
@@ -0,0 +1,86 @@
+package com.plb.employeemgt.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Task entity
+ */
+@Entity
+@Table(name = "task")
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taskSequenceGenerator")
+    @SequenceGenerator(name = "taskSequenceGenerator")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToMany(mappedBy = "tasks")
+    private Set<Job> jobs = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(Set<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(title, task.title) &&
+                Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", description='" + getDescription() + "'" +
+            "}";
+    }
+}
